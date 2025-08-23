a90f2546cba00dba72fa8f00b2de1a3cb83784db
jivancevic <josip.ivancevic@fer.hr>
2025-08-23 14:49:17 +0200

    Setup Github Workflow to automatically deploy server to DigitalOcean.
    Try to fix deployment workflow.
    
    Changed node version to 24.
    
    First rsync then restart PM2.
    
    Changed PM2 config to CJS.
    
    Fixed PM2 config.
    
    Fixed PM2 config.

---
+++ server/ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "apartments-ivancevic-server",
+      script: "dist/server.js",
+      instances: "max",
+      exec_mode: "cluster",
+      autorestart: true,
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
